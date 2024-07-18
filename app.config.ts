02e25f11179f36e4aa11f0798b65b502bbad6abe
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-07-18 16:47:16 -0400

    Update version number

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Back On Track",
   slug: "bot-app",
-  version: "1.0.2",
+  version: "1.0.3",
   orientation: "portrait",
   icon: "./src/assets/icon.png",
   userInterfaceStyle: "automatic",
